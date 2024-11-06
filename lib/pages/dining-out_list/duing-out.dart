db4bb080b03d418deb3c67dc77a09c1d54bb098b
eita <>
2024-11-06 15:23:08 +0900

    デザイン作成

--- lib/pages/dining-out_list/duing-out.dart
+++ lib/pages/dining-out_list/duing-out.dart
@@ -8,18 +8,30 @@ class Duing_out extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('外食リスト')),
+        appBar: AppBar(
+          title: const Text('外食履歴'),
+          backgroundColor: const Color(0xFFB0E0E6),
+        ),
         body: Column(
           children: [
             Expanded(
-                child: ListView.builder(
-                    itemCount: ListItem.duingList.length,
-                    itemBuilder: (context, index) => Column(children: [
-                          ListTile(
-                            title: Text(ListItem.duingList[index]),
-                            onTap: () {},
-                          ),
-                        ])))
+              child: ListView.builder(
+                itemCount: ListItem.duingList.length,
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 8.0, horizontal: 16.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFB0E0F6),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: ListTile(
+                      title: Text(ListItem.duingList[index]),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ));
   }
