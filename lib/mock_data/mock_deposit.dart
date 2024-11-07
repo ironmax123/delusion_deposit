c272416f6d7ab0c6392e52d16ef125affe4ca185
eita <>
2024-11-08 08:22:24 +0900

    モックデータ追加

---
+++ lib/mock_data/mock_deposit.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<void> mockDeposit() async {
+  final prefs = await SharedPreferences.getInstance();
+
+  // データを直接SharedPreferencesに保存
+  await prefs.setString(
+    'saved_deposit',
+    jsonEncode([
+      {
+        "deposit": "1000",
+      } //毎週の使用金額
+    ]),
+  );
+
+  print('データが端末に保存されました');
+}
