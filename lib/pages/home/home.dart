cde61bf205d9caf2f7e9112cfb456b0b3eba81ad
sotogumi <u961841f@ecs.osaka-u.ac.jp>
2024-11-07 15:15:30 +0900

    homeUIの基盤完成

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -18,88 +18,112 @@ class HomePage extends StatelessWidget {
         body: Center(
           child: Column(
             children: [
-              const Padding(
-                padding: EdgeInsets.fromLTRB(16, 40, 16, 0),
-                child: ColoredBox(
-                  color: Colors.blue,
-                  child: SizedBox(
-                    height: 160,
-                    width: 400,
-                    child: Column(
-                      children: [
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Padding(
-                            padding: EdgeInsets.fromLTRB(8, 8, 0, 0),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 40, 16, 0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: const ColoredBox(
+                    color: Colors.blue,
+                    child: SizedBox(
+                      height: 160,
+                      width: 400,
+                      child: Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(8, 8, 0, 0),
+                              child: Text(
+                                '貯金額',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Align(
                             child: Text(
-                              '貯金額',
+                              '10000円',
                               style: TextStyle(
-                                fontSize: 20,
+                                fontSize: 50,
                                 fontWeight: FontWeight.bold,
                               ),
                             ),
                           ),
-                        ),
-                        Align(
-                          child: Text(
-                            '10000円',
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: Padding(
+                              padding: EdgeInsets.only(right: 8),
+                              child: Text(
+                                'あと99990000円',
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16, 16, 16, 32),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(30),
+                  child: const ColoredBox(
+                    color: Colors.blue,
+                    child: SizedBox(
+                      height: 160,
+                      width: 400,
+                      child: Column(
+                        children: [
+                          Text(
+                            '目標',
                             style: TextStyle(
-                              fontSize: 56,
+                              fontSize: 20,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-                        ),
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: Padding(
-                            padding: EdgeInsets.only(right: 8),
-                            child: Text(
-                              'あと99990000円',
-                              style: TextStyle(
-                                fontSize: 30,
-                                fontWeight: FontWeight.bold,
-                              ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 20),
+                            child: Row(
+                              children: [
+                                Text(
+                                  '11/2',
+                                  style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'までに',
+                                  style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  '沖縄の別荘',
+                                  style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.fromLTRB(16, 16, 16, 32),
-                child: ColoredBox(
-                  color: Colors.blue,
-                  child: SizedBox(
-                    height: 160,
-                    width: 400,
-                    child: Column(
-                      children: [
-                        Text('目標'),
-                        Row(
-                          children: [
-                            Text('11/2までに'),
-                            Text('沖縄の別荘'),
-                          ],
-                        ),
-                        Text('1000000000円'),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              const ColoredBox(
-                color: Colors.blue,
-                child: SizedBox(
-                  height: 80,
-                  width: 240,
-                  child: Center(
-                    child: Text(
-                      '外食履歴遷移',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
+                          Text(
+                            '1000000000円',
+                            style: TextStyle(
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ),
