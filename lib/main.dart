effeb4c5f9d3e5c7463636ec860f172f1f0427b1
sotogumi <u961841f@ecs.osaka-u.ac.jp>
2024-11-08 21:13:27 +0900

    目標設定による条件分岐

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:delusion_deposit/pages/target-input/target_input.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'pages/home/home.dart';
 
@@ -16,7 +18,31 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: FutureBuilder<bool>(
+        future: checkTarget(),
+        builder: (context, snapshot) {
+          // ロード中の場合に表示するウィジェット
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          // 目標が設定されているかどうかで遷移先を決定
+          if (snapshot.hasData && snapshot.data == true) {
+            return const HomePage(); // 目標が設定されていればHomePageを表示
+          } else {
+            return const TargetInput(); // 目標が設定されていなければTargetInputを表示
+          }
+        },
+      ),
     );
+  }
+
+  // 目標が設定されているかどうかをSharedPreferencesで確認
+  Future<bool> checkTarget() async {
+    final prefs = await SharedPreferences.getInstance();
+    // SharedPreferencesに保存された目標情報を確認
+    bool isGoalSet =
+        prefs.getBool('is_goal_set') ?? false; // 'is_goal_set'のキーで設定状態を確認
+    return isGoalSet;
   }
 }
